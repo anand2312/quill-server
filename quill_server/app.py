d1c8ae6a124225ff482c411791902be3d7e95b75
anand2312 <40204976+anand2312@users.noreply.github.com>
2023-11-08 00:01:41 +0530

    fix: include user router

--- quill_server/app.py
+++ quill_server/app.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 
 from quill_server.schema import MessageResponse
+from quill_server.routers import user
 
 app = FastAPI(title="Quill")
+
+app.include_router(user.router)
 
 
 @app.get("/ping")
